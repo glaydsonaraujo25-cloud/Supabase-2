a4b2643e2ef46b90bb6c3c0145ce10cf7dcd9ab1
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 14:19:10 -0300

    feat: mount batch schedule tools

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App'
 import AdminToolkit from './AdminToolkit'
 import OperationalCenter from './OperationalCenter'
 import AdministrationCenter from './AdministrationCenter'
+import ScheduleBatchTools from './ScheduleBatchTools'
 import './styles.css'
 import './enhancements.css'
 import './admin-toolkit.css'
 import './operational-center.css'
 import './administration-center.css'
+import './schedule-batch-tools.css'
 import './serviceAdminEnhancements'
 
 createRoot(document.getElementById('root')!).render(
@@ -18,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
       <AdminToolkit />
       <OperationalCenter />
       <AdministrationCenter />
+      <ScheduleBatchTools />
     </>
   </StrictMode>,
 )
