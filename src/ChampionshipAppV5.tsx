c98468913cd89dd4d82e5d67ff09e9ae2ad81617
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 15:29:55 -0300

    refactor: mount statistics center

---
+++ src/ChampionshipAppV5.tsx
@@ -0,0 +1,6 @@
+import ChampionshipAppV4 from './ChampionshipAppV4'
+import StatisticsCenter from './StatisticsCenter'
+
+export default function ChampionshipAppV5(){
+  return <><ChampionshipAppV4/><StatisticsCenter/></>
+}
