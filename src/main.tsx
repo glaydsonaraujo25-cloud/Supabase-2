37d315b190ac99836678829dcd865757826eea72
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 14:13:08 -0300

    feat: mount service acknowledgement center

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App'
 import AdminToolkit from './AdminToolkit'
 import OperationalCenter from './OperationalCenter'
 import AdministrationCenter from './AdministrationCenter'
+import ServiceAcknowledgements from './ServiceAcknowledgements'
 import './styles.css'
 import './enhancements.css'
 import './admin-toolkit.css'
 import './operational-center.css'
 import './administration-center.css'
+import './service-acknowledgements.css'
 import './serviceAdminEnhancements'
 
 createRoot(document.getElementById('root')!).render(
@@ -18,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
       <AdminToolkit />
       <OperationalCenter />
       <AdministrationCenter />
+      <ServiceAcknowledgements />
     </>
   </StrictMode>,
 )
