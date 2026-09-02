c8d38d022c744d83618ce2721dcc31abb064d148
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 13:55:51 -0300

    feat: enable service type admin actions

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './enhancements.css'
+import './serviceAdminEnhancements'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
