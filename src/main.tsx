9fcbd110b3385ab9267914f2f036e63e76f7b295
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 15:30:21 -0300

    refactor: load statistics-enabled championship app

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ChampionshipAppV4 from './ChampionshipAppV4'
+import ChampionshipAppV5 from './ChampionshipAppV5'
 import PublicChampionship from './PublicChampionship'
 import './championship-app.css'
 import './multiuser.css'
 import './participant-admin.css'
 import './public-sharing.css'
+import './statistics.css'
 
 const publicSlug = new URLSearchParams(location.search).get('public')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {publicSlug ? <PublicChampionship slug={publicSlug} /> : <ChampionshipAppV4 />}
+    {publicSlug ? <PublicChampionship slug={publicSlug} /> : <ChampionshipAppV5 />}
   </StrictMode>,
 )
