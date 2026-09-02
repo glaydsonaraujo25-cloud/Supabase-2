af22ccfde0acb810be8eda21963fd66af5ab981b
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 15:02:34 -0300

    refactor: mount public championship sharing flow

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ChampionshipAppV3 from './ChampionshipAppV3'
+import ChampionshipAppV4 from './ChampionshipAppV4'
+import PublicChampionship from './PublicChampionship'
 import './championship-app.css'
 import './multiuser.css'
 import './participant-admin.css'
+import './public-sharing.css'
+
+const publicSlug = new URLSearchParams(location.search).get('public')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChampionshipAppV3 />
+    {publicSlug ? <PublicChampionship slug={publicSlug} /> : <ChampionshipAppV4 />}
   </StrictMode>,
 )
