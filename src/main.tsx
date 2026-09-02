cc2d1483442e147bb4802e541f572cce8eb17ee9
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 14:15:08 -0300

    revert: remove service acknowledgement UI

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,11 @@ import App from './App'
 import AdminToolkit from './AdminToolkit'
 import OperationalCenter from './OperationalCenter'
 import AdministrationCenter from './AdministrationCenter'
-import ServiceAcknowledgements from './ServiceAcknowledgements'
 import './styles.css'
 import './enhancements.css'
 import './admin-toolkit.css'
 import './operational-center.css'
 import './administration-center.css'
-import './service-acknowledgements.css'
 import './serviceAdminEnhancements'
 
 createRoot(document.getElementById('root')!).render(
@@ -20,7 +18,6 @@ createRoot(document.getElementById('root')!).render(
       <AdminToolkit />
       <OperationalCenter />
       <AdministrationCenter />
-      <ServiceAcknowledgements />
     </>
   </StrictMode>,
 )
