c7aaa6d1dc954c0ebfbc5bd8e7c580b598bee62b
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 14:00:15 -0300

    feat: integrate admin toolkit into application

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import AdminToolkit from './AdminToolkit'
 import './styles.css'
 import './enhancements.css'
+import './admin-toolkit.css'
 import './serviceAdminEnhancements'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      <AdminToolkit />
+    </>
   </StrictMode>,
 )
