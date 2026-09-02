5b045c74ea9cbee517158d1705359d221096635c
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 14:49:19 -0300

    style: load multiuser championship styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ChampionshipAppV2 from './ChampionshipAppV2'
 import './championship-app.css'
+import './multiuser.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
