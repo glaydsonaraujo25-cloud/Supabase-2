118cc26d031583d3352c70c46ea09e4013fec68f
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 14:59:26 -0300

    refactor: mount participant administration center

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ChampionshipAppV2 from './ChampionshipAppV2'
+import ChampionshipAppV3 from './ChampionshipAppV3'
 import './championship-app.css'
 import './multiuser.css'
+import './participant-admin.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChampionshipAppV2 />
+    <ChampionshipAppV3 />
   </StrictMode>,
 )
