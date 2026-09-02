170bb466657d3596e645608947fe91bba609a084
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 15:45:45 -0300

    refactor: load knockout-enabled championship app

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ChampionshipAppV5 from './ChampionshipAppV5'
+import ChampionshipAppV6 from './ChampionshipAppV6'
 import PublicChampionship from './PublicChampionship'
 import './championship-app.css'
 import './multiuser.css'
 import './participant-admin.css'
 import './public-sharing.css'
 import './statistics.css'
+import './knockout.css'
 
 const publicSlug = new URLSearchParams(location.search).get('public')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {publicSlug ? <PublicChampionship slug={publicSlug} /> : <ChampionshipAppV5 />}
+    {publicSlug ? <PublicChampionship slug={publicSlug} /> : <ChampionshipAppV6 />}
   </StrictMode>,
 )
