66e59efaa4ea5215928426545a057ec9ff662946
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 14:03:47 -0300

    feat: mount operational center

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import AdminToolkit from './AdminToolkit'
+import OperationalCenter from './OperationalCenter'
 import './styles.css'
 import './enhancements.css'
 import './admin-toolkit.css'
+import './operational-center.css'
 import './serviceAdminEnhancements'
 
 createRoot(document.getElementById('root')!).render(
@@ -12,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <>
       <App />
       <AdminToolkit />
+      <OperationalCenter />
     </>
   </StrictMode>,
 )
