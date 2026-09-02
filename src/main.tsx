f92c86c68633396332ea314e38334eef3eb31250
Glaydson de Araujo <glaydsonaraujo25@gmail.com>
2026-09-02 14:35:07 -0300

    refactor: load only championship manager

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import AdminToolkit from './AdminToolkit'
-import OperationalCenter from './OperationalCenter'
-import AdministrationCenter from './AdministrationCenter'
-import ScheduleBatchTools from './ScheduleBatchTools'
 import './styles.css'
-import './enhancements.css'
-import './admin-toolkit.css'
-import './operational-center.css'
-import './administration-center.css'
-import './schedule-batch-tools.css'
-import './serviceAdminEnhancements'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <>
-      <App />
-      <AdminToolkit />
-      <OperationalCenter />
-      <AdministrationCenter />
-      <ScheduleBatchTools />
-    </>
+    <App />
   </StrictMode>,
 )
